559f7c5929cdf0049dfa7fadf3d857dc8541387b
riteshbenjwal <riteshbenjwal7@gmail.com>
2022-10-29 13:50:51 +0530

    Intro to typescript

---
+++ Sec-19 TypeScript/fetchjson/index.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+const url = "https://jsonplaceholder.typicode.com/todos/1";
+
+//Structure of Object
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+  logTodo(id, title, completed);
+});
+
+//Checking Type of  variable - saves us from calling a function with incorrect order of arguments
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(
+    `
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+    `
+  );
+};
