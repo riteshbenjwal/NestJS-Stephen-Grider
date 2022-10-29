ea6efa4b1bbb346e3d2d65d9adbf8490f1b6ef1f
riteshbenjwal <riteshbenjwal7@gmail.com>
2022-10-29 17:27:21 +0530

    Annotations with functions and objects

---
+++ Sec-19 TypeScript/3 Annotations with functions and objects/1 More on Annotations Around Functions.ts
@@ -0,0 +1,5 @@
+// Functions
+
+const add = (a: number, b: number): number => {
+  return a + b;
+};
---
+++ Sec-19 TypeScript/3 Annotations with functions and objects/2 Inference Around Functions.ts
@@ -0,0 +1,8 @@
+// Functions
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+const sub = (a: number, b: number): number => {
+  return a - b;
+};
