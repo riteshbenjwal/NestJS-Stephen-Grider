5c7d7724a1f76195ae2d70c59df266e80ff8d97e
riteshbenjwal <riteshbenjwal7@gmail.com>
2022-12-04 19:16:30 +0530

    user crud

--- mycv_sec-7-to-18/src/users/users.service.ts
+++ mycv_sec-7-to-18/src/users/users.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+    return this.repo.save(user);
+  }
+
+  findOne(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.repo.find({ where: { email } });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    console.log('service', id);
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+    return this.repo.remove(user);
+  }
+}
+
+/*
+const userService = new UsersService({} as any);
+usersService.update(1,{email: 'abc@gmail.com'})
+usersService.update(1,{email: 'abc@gmail.com', password: '12445frfg' })
+usersService.update(1,{password:'12445frfg'})
+*/
